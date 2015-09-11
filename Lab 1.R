d353e119fcb74aafbbc88eef9b7e0cb5107dded5
TheKevinNeville <kevin@neville.se>
2015-09-11 08:43:33 +0200

    R-code for assignment 1 and 2.

---
+++ Lab 1.R
@@ -0,0 +1,92 @@
+rhine <- read.csv2("/Users/Kevin/Documents/Skola/732A34 - Time series/Lab 1/Rhine.csv")
+library(TSA)
+rhine <- ts(rhine, freq = 12, start = 1)
+
+# 1. a)
+# Plot data with months marked on the time series.
+# plot(time(rhine), rhine[, 4], type = "l",ylab = "Nitrogen", main = "Monthly concentrations of total nitrogen in the Rhine river")
+# points(rhine[,4], pch=as.vector(season(rhine)))
+
+# 1.b)
+# Estimate linear model and name it model_b
+b_model <- lm(rhine[,4]~time(rhine))
+# summary(b_model)
+
+# Plot the ACF
+# acf(rstudent(b_model))
+
+# c)
+# Plottar data med regression.
+# plot(time(rhine), rhine[, 4], type = "l",ylab = "Nitrogen", main = "Monthly concentrations of total nitrogen in the Rhine river")
+# points(rhine[,4], pch=as.vector(season(rhine)))
+# abline(b_model, col = "red")
+
+# Skapar en modell med dummyvariabler för varje månad.
+b_month <- season(rhine[, 4])
+b_model_month <- lm(rhine[, 4] ~ b_month + time(rhine))
+# summary(b_model_month)
+
+# Plottar residualerna för b_model_month_resid
+b_model_month_resid <- rstudent(b_model_month)
+# plot(b_model_month_resid, type="l")
+# points(b_model_month_resid, pch=as.vector(season(rhine)))
+
+# Plottar ACF för b_model_month
+# acf(rstudent(b_model_month))
+
+# Plottar QQ plott
+# qqnorm(b_model_month_resid)
+# qqline(b_model_month_resid)
+
+
+# 1.d.a
+data(retail)
+# plot(time(retail), retail, type = "l",ylab = "Sales", main = "Monthly total UK retail sales in billions of pounds")
+# points(retail, pch=as.vector(season(retail)))
+
+# 1.d.b
+d_b_model <- lm(retail~time(retail))
+# summary(d_b_model)
+
+# plottar ACF
+# acf(rstudent(d_b_model))
+
+# 1.d.c
+# Skapar plot
+# plot(time(retail), retail, type = "l",ylab = "Sales", main = "Monthly total UK retail sales in billions of pounds")
+# points(retail, pch=as.vector(season(retail)))
+# abline(d_b_model, col = "red")
+
+# Skapar model med dummy
+d_b_month <- season(retail)
+d_b_model2 <- lm(retail ~ d_b_month + time(retail))
+# summary(d_b_model2)
+
+# Plottar residualerna
+d_b_model2_resid <- rstudent(d_b_model2)
+# plot(d_b_model2_resid, type="l")
+# points(d_b_model2_resid, pch=as.vector(season(retail)))
+
+# ACF for the new model
+# acf(rstudent(d_b_model2))
+
+# qqPlot
+# qqnorm(rstudent(d_b_model2)); qqline(rstudent(d_b_model2))
+
+
+# Uppgift 2
+AR2sim <- function(T, phi1, phi2){
+  y <- rnorm(T)
+  j <- y
+  y[2] <-  phi1*y[1] + j[2]
+  for (i in 3:T){
+    y[i] <- phi1*y[i-1]+phi2*y[i-2] + j[i]
+  }
+  # plot(y, type= "l")
+  acf(y)
+  #var(y)
+}
+
+# AR2sim(T=100,0.8,0)
+# AR2sim(T=100,-0.5,0.5)
+# AR2sim(T=100, 0, -0.64)
